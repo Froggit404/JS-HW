2d4c843d7fbcfafb2d0e8526a63f7e436e3ae5df
Froggit404 <hrytsykdiplom@gmail.com>
2025-02-10 22:19:00 +0100

    Зробив перше дз

---
+++ HW-1/JS-1_1.js
@@ -0,0 +1,62 @@
+let a = 'hello'
+console.log(a)
+
+let b = 'owu'
+console.log(b)
+
+let c = 'com'
+console.log(c)
+
+let d = 'ua'
+console.log(d)
+
+let e = 1
+console.log(e)
+
+let f = 10
+console.log(f)
+
+let g = -999
+console.log(g)
+
+let h = 123
+console.log(h)
+
+const PI = 3.14
+console.log(PI)
+
+let i = 2.7
+console.log(i)
+
+let j = 16
+console.log(j)
+
+let k = true
+console.log(k)
+
+let l = false
+console.log(l)
+
+//----------------------------------------------------
+
+let firstName = 'Dmytro'
+let lastName = 'Hrytsyk'
+let middleName = 'Igorovych'
+let person = `${firstName} ${lastName} ${middleName}`
+console.log(person)
+console.log(firstName+' '+lastName+' '+middleName)
+
+
+
+
+//-----------------------------------------------------------
+
+let m = 100;
+let n = '100';
+let o = true;
+
+console.log(typeof m)
+console.log(typeof n)
+console.log(typeof o)
+
+
