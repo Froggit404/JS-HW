a3d1eed6347abc4395f691b3725263587a9b46c8
Froggit404 <hrytsykdiplom@gmail.com>
2025-02-13 00:46:28 +0100

    Виконую 4 ДЗ

---
+++ HW-4/JS-4.js
@@ -0,0 +1,43 @@
+// {
+//     function rectangle(length, height) {
+//         return length * height
+//     }
+//
+//     let square = rectangle(3, 8)
+//     console.log(square)
+//
+// }
+
+//- створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
+
+// {
+//     function circle(radius) {
+// return Math.PI * radius * radius
+//     }
+//     let square = circle(14)
+//     console.log(square)
+// }
+
+
+// - створити функцію яка обчислює та повертає площу кола з радіусом r
+
+
+// {
+//
+//     // Формула для обрахунку площі повної поверхні циліндра
+//     //
+//     // S = 2 π R h + 2 π R 2   =    2 π R(R + h)
+//
+//     function cylinder(height, radius) {
+//         let radiusHeight= radius + height
+//         return 2*Math.PI*radius*radiusHeight
+//     }
+//     let square = cylinder(22, 13)
+//     console.log(square)
+//
+// }
+
+
+
+
+// - створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r
